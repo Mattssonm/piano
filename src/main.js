ba3a1b1969dee848e178b6edcc875e1c3eb5f807
Mattssonm <markusmattsson90@gmail.com>
2020-10-29 11:43:41 +0100

    Add keydown eventListener

--- src/main.js
+++ src/main.js
@@ -2,22 +2,36 @@ window.addEventListener("load", () => {
 
   // --- variables ---
   const samples = {};
-  const notes = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"];
+  const notesAndButtons = [
+    ["C", "d"],
+    ["Db", "r"],
+    ["D", "f"],
+    ["Eb", "t"],
+    ["E", "g"],
+    ["F", "h"],
+    ["Gb", "u"],
+    ["G", "j"],
+    ["Ab", "i"],
+    ["A", "k"],
+    ["Bb", "o"],
+    ["B", "l"],
+  ];
 
   // --- functions ---
 
   //create the keyboard inside specified div
   function createKeyboard(div) {
     //connect each note with an audio sample in the samples object
-    notes.forEach( note => {
-      samples[note] = new Audio(`./samples/${note}.wav`);
+    notesAndButtons.forEach( note => {
+      samples[note[0]] = new Audio(`./samples/${note[0]}.wav`);
     }); 
-    
-    //create buttons for all the notes
-    notes.forEach( note => {
+
+    //create buttons for all the notesAndButtons
+    notesAndButtons.forEach( note => {
       const element = document.createElement("button")
-      element.setAttribute("data-note", note);
-      note.length === 1 ? element.classList.add("key", "whiteKey") : element.classList.add("key", "blackKey");
+      element.setAttribute("data-note", note[0]);
+      element.setAttribute("data-button", note[1]);
+      note[0].length === 1 ? element.classList.add("key", "whiteKey") : element.classList.add("key", "blackKey");
       div.appendChild(element);  
     });
   }
@@ -25,7 +39,7 @@ window.addEventListener("load", () => {
   //create labels for each key
   function createLabels() { 
     keys.forEach( key => {
-      const cords = key.getClientRects()[0];
+      const cords = key.getBoundingClientRect();
       const note = key.getAttribute("data-note");
 
       let noteLabel = document.createElement("label");
@@ -70,12 +84,12 @@ window.addEventListener("load", () => {
     return newElement;
   }
 
-  function keyPressed(event, key) {
+  function keyPressed(key) {
     playNote(key.getAttribute("data-note"));
-      
-      //get coordinates of clicked element and send them with the animateKey function
-      const cords = event.target.getClientRects()[0];
-      animateKey(key, cords.left, cords.top);
+    
+    //get coordinates of element and pass as arguments to the animateKey function
+    const cords = key.getBoundingClientRect();
+    animateKey(key, cords.left, cords.top);
   }
 
   // --- program start ---
@@ -86,15 +100,24 @@ window.addEventListener("load", () => {
 
   //create labels
   const keys = document.querySelectorAll(".key");
+  console.log(keys);
   createLabels();
 
   // --- event listeners --- 
 
-  //add listener to all keys that calls playNote on click 
+  //add listener to all keys that calls playNote on click and on button press
   keys.forEach( key => {
     key.addEventListener("click", e => {
-      keyPressed(e, key);
+      keyPressed(key);
     });
   });
+
+  document.addEventListener("keydown", function(e) {
+    keys.forEach( key => {
+      if(key.getAttribute("data-button") === e.key) {
+        keyPressed(key);
+      }
+    });
+  })
 
 });
